16cb058a8c89f01d08f44c55374b76410ac54dca
AS-Coder26 <ascoder26@gmail.com>
2019-10-16 11:52:21 +0530

    TECH CHALLENGE FIRST COMMIT

--- computerstrategy.py
+++ computerstrategy.py
@@ -1,8 +1,6 @@
 import random
 def strategy(player_number_list):
 	temp = []
-	#print("in function")
-	#print(player_number_list)
 	if not player_number_list:
 		return random.randrange(1,3)
 	else:
@@ -24,12 +22,12 @@ if __name__ == '__main__':
 	my_point = 0
 	player_number_list = []
 	while player_point < 5 and my_point < 5:
-		player_number = int(input("Enter any number"))
+		player_number = int(input("Enter any number: "))
 		my_number = strategy(player_number_list)
 		player_number_list.append(player_number)
 
-		print("player_number",player_number)
-		print("my_number",my_number)
+		print("Your Number: ",player_number)
+		print("Computer Number: ",my_number)
 		
 		if player_number +1 == my_number:
 			my_point += 2
@@ -40,11 +38,11 @@ if __name__ == '__main__':
 		elif my_number+1 == player_number:
 			player_point += 2
 		
-		print("player_point",player_point)
-		print("my_point",my_point)
+		print("Your Point",player_point)
+		print("Computer Point",my_point)
 
 	if my_point > player_point:
-		print("My strategy wins")
+		print("Computer Strategy Wins")
 	else:
 		print("You win")
 
