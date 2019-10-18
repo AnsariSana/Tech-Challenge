633df691344e18dde26591a03b5e7be8fd691b72
AS-Coder26 <ascoder26@gmail.com>
2019-10-18 21:28:50 +0530

    TECH CHALLENGE WITH COMMENTED CODE

--- computerstrategy.py
+++ computerstrategy.py
@@ -5,12 +5,12 @@ def strategy(player_number_list):					#strategy function to choose number using
 	if not player_number_list:					#Initially when there is no previous input from
 		return random.randrange(1,3)				#opponent side then the smallest possible number will be returned.
 	else:
-			if player_number_list[-1] >= 3:		#If last element entered by the user is 3 or greater then
+			if player_number_list[-1] >= 3:			#If last element entered by the user is 3 or greater then
 									#that then return smallest possible number
-				player_number_list.pop()				#pop the top most element to check if further opponent
-														#may use only smallest numbers
-				return random.choice([1,3,4])			#here choice method is used because if it return
-														#2 and opponent will enter 3 then opponent will gain 2 points.
+				player_number_list.pop()			#pop the top most element to check if further opponent
+													#may use only smallest numbers
+				return random.choice([1,3,4])		#here choice method is used because if it return
+													#2 and opponent will enter 3 then opponent will gain 2 points.
 
 			elif len(set(player_number_list) - {1,2}) == 0:		#If opponent continuously trying to use either 1
 				return random.randrange(2,4)					#or 2 or 1,2 then return 2,3 
